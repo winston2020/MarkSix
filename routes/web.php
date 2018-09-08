a5e542a20e38bce9a3c28b27568223938abf07eb
Handsome Boys <1094692718@qq.com>
2018-09-08 15:13:55 +0800

    's'

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('','HomeController@index');
+Route::get('active','HomeController@index');
+Route::get('reward;','HomeController@index');
+Route::get('money','HomeController@index');
+Route::get('user','HomeController@index');
